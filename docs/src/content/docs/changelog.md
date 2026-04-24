2ae1c04dcc2b757ae79d2c81703adb39704ed9ed
HumanUser <is.homutov@gmail.com>
2026-04-24 23:54:47 +0200

    chore: bump to 0.0.55

--- docs/src/content/docs/changelog.md
+++ docs/src/content/docs/changelog.md
@@ -3,6 +3,6 @@ title: Changelog
 ---
 
 ## 2026.04.24
-Version: **0.0.54**
+Version: **0.0.55**
 
 Initial public release.
--- docs/src/data/flyql-examples.ts
+++ docs/src/data/flyql-examples.ts
@@ -1,4 +1,4 @@
-export const flyqlVersion = '0.0.54'
+export const flyqlVersion = '0.0.55'
 
 export const landingIntroQuery = "status >= 400 and host like 'prod%' and not debug"
 
